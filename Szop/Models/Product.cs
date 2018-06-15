2ad2c5b337d6ac56a95f294aecbdd217086493fc
Zofia Franczyk <zosia.franczyk@gmail.com>
2018-06-15 14:26:52 +0200

    add class Category and modify Product

--- Szop/Models/Product.cs
+++ Szop/Models/Product.cs
@@ -10,7 +10,7 @@ namespace Szop.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string Category { get; set; }
+        public Category Category { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
     }
