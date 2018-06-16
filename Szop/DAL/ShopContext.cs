28a854e68bfd0810fc7f3ebe71f9e748689b5d3a
Maciej Znamirowski <maciek@znamirowski.pl>
2018-06-16 15:47:47 +0200

    Some nonfunctional changes related to db.

---
+++ Szop/DAL/ShopContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Szop.DAL
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext() : base("ShopContext") {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        //public DbSet<>
+    }
+}
---
+++ Szop/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Szop.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
