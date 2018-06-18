b00ec8624fd4eee64bf13b5a339e9b68db861426
zosiaef <zosia.franczyk@gmail.com>
2018-06-18 16:42:27 +0200

    Update CartsController.cs

--- Szop/Controllers/CartsController.cs
+++ Szop/Controllers/CartsController.cs
@@ -1,60 +1,87 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
+using Szop.Authentication;
 using Szop.DAL;
 using Szop.DBModels;
 using Szop.Models;
 
 namespace Szop.Controllers
 {
+    [JWTAuthenticationFilter]
     public class CartsController : ApiController
     {
         private ShopContext db = new ShopContext();
-        Cart[] carts = new Cart[]
-        {
-           new Cart {
-            User = new User {Id = 1, Email = "aaa", PassHash = "sss" },
-            Product = new Product {
-                Id = 2,
-                Name = "Yo-yo",
-                Price = 3.75M,
-                Category = new Category { Id = 2, Name = "Toys" },
-                Image = "adgsrvjtchsegc4w5gc",
-                Description = "funny toys"
-                }
-            }
-        };
+
         //get add delete buy
         // POST <controller>
-        public int Add(int Id)
+        public int Add([FromBody]DBCart value)
         {
-            return Id;
+            var authenticationIdentity = Thread.CurrentPrincipal.Identity as JWTAuthenticationIdentity;
+            int UserId = authenticationIdentity.UserId;
+            DBCart toAdd = new DBCart
+            {
+                Id = value.Id,
+                UserId = UserId,
+                ProductId = value.ProductId,
+                Quantity = value.Quantity
+            };
+            DBCart added = db.Carts.Add(toAdd);
+            db.SaveChanges();
+
+            return added.Id;
         }
 
         // GET <controller>
         public IEnumerable<Cart> Get()
         {
-            return carts;
+            var authenticationIdentity = Thread.CurrentPrincipal.Identity as JWTAuthenticationIdentity;
+            int userId = authenticationIdentity.UserId;
+            return db.Carts.AsEnumerable().Where(x => x.UserId == userId).Select(p => Map(p));
         }
 
-        public int delete(int id)
+
+        // DELETE carts/{productId}
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
         {
-            return 1;
+            var authenticationIdentity = Thread.CurrentPrincipal.Identity as JWTAuthenticationIdentity;
+            int userId = authenticationIdentity.UserId;
+
+            DBCart cart = db.Carts.Where(c =>
+                c.UserId == userId &&
+                c.ProductId == id
+            ).First();
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            db.Carts.Remove(cart);
+            db.SaveChanges();
+
+            return Ok(cart);
         }
 
         internal Cart Map(DBCart dbCart)
         {
             DBProduct prod = db.Products.Find(dbCart.ProductId);
-            DBUser user = db.Users.Find(dbCart.UserId);
+
+            if (prod == null)
+                return null;
+            
             DBCategory c = db.Categories.Find(prod.CategoryId);
+
             if (dbCart == null)
                 return null;
+
             return new Cart()
             {
-                Id = dbCart.Id,
-                User = new User {Id = user.Id, Email = user.Email, PassHash = user.PassHash },
                 Product = new Product
                 {
                     Id = prod.Id,
@@ -68,15 +95,5 @@ namespace Szop.Controllers
             };
         }
 
-        internal DBCart InverseMap(Cart cart)
-        {
-            return new DBCart()
-            {
-                Id = cart.Id,
-                UserId = cart.User.Id,
-                ProductId = cart.Product.Id,
-                Quantity = cart.Quantity
-            };
-        }
     }
 }
