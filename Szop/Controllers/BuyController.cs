76efe835e145bde1953876e990a2e7576c389d4d
Maciej Znamirowski <maciek@znamirowski.pl>
2018-06-18 18:14:02 +0200

    Added /buy endpoint.

--- Szop/Controllers/BuyController.cs
+++ Szop/Controllers/BuyController.cs
@@ -1,16 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
+using Szop.Authentication;
+using Szop.DAL;
+using Szop.DBModels;
 
 namespace Szop.Controllers
 {
+    [JWTAuthenticationFilter]
     public class BuyController : ApiController
     {
-        public int buyProducts()
+        private ShopContext db = new ShopContext();
+
+        public HttpResponseMessage BuyProducts()
         {
-            return 1;
+            var authenticationIdentity = Thread.CurrentPrincipal.Identity as JWTAuthenticationIdentity;
+            int userId = authenticationIdentity.UserId;
+
+            IEnumerable<DBCart> crds = db.Carts.Where(c =>
+                c.UserId == userId
+            );
+
+            if (crds.Count() == 0)
+                return Request.CreateResponse(HttpStatusCode.OK);
+
+            crds.ToList().ForEach(p => db.Carts.Remove(p));
+            db.SaveChanges();
+
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
 
     }
